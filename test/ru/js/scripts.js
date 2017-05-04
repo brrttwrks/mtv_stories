22996b2396f37c0b6ca7b3ae7b58bd9684111365
Eric Barrett <eric@barrett.works>
2017-05-04 14:19:19 +0400

    update stories

--- test/ru/js/scripts.js
+++ test/ru/js/scripts.js
@@ -1,11 +1,33 @@
 $(document).ready(function() {
-  var viewportHeight = $(window).height();
-  var videoHeight = parseInt(viewportHeight) * 0.80;
+  var viewportWidth = $(window).width();
+  var videoHeight = viewportWidth * 0.338;
   $('iframe').attr('height', videoHeight);
 });
 
-$(window).resize(function(){
-  var viewportHeight = $(window).height();
-  var videoHeight = parseInt(viewportHeight) * 0.80;
-  $('iframe').attr('height', videoHeight);
-}
+
+$(window).on('resize', function(){
+      var win = $(this);
+
+      if (win.width() > 1024) {
+        $('iframe').attr('width', '40vw');
+        var videoWidth = 40;
+      }	elseif (win.width() > 850) {
+        $('iframe').attr('width', '50vw');
+        var videoWidth = 50;
+      } elseif (win.width() > 600) {
+        $('iframe').attr('width', '70vw');
+        var videoWidth = 70;
+      } elseif (win.width() > 400) {
+        $('iframe').attr('width', '80vw');
+        var videoWidth = 80;
+      } elseif (win.width() > 300) {
+        $('iframe').attr('width', '90vw');
+        var videoWidth = 90;
+      } else {
+        $('iframe').attr('width', '90vw');
+        var videoWidth = 40;
+      }
+
+      var videoHeight =  videoWidth * 0.338;;
+      $('iframe').attr('height', videoHeight + 'vh');
+});
