3e15038e3f0466474d5beab516a64a53f4edc8cb
Eric Barrett <eric@barrett.works>
2017-05-04 09:12:51 +0400

    update ilgar story

--- test/ru/js/scripts.js
+++ test/ru/js/scripts.js
@@ -3,3 +3,9 @@ $(document).ready(function() {
   var videoHeight = parseInt(viewportHeight) * 0.80;
   $('iframe').attr('height', videoHeight);
 });
+
+$(window).resize(function(){
+  var viewportHeight = $(window).height();
+  var videoHeight = parseInt(viewportHeight) * 0.80;
+  $('iframe').attr('height', videoHeight);
+}
